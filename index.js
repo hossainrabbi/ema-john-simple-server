71975da43d1f1cf2b1ade85d29ec792b6591eb31
Hossain <hossainrabbi259@gmail.com>
2021-03-29 11:20:47 +0600

    something changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kiqx3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
